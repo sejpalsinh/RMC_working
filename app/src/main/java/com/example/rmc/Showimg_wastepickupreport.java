3abb40e052149b6398b18b1a884c52a81138a5c7
sejpalsinh <55375017+sejpalsinh@users.noreply.github.com>
2020-01-13 10:37:41 +0530

    report activity

---
+++ app/src/main/java/com/example/rmc/Showimg_wastepickupreport.java
@@ -0,0 +1,14 @@
+package com.example.rmc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Showimg_wastepickupreport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showimg_wastepickupreport);
+    }
+}
